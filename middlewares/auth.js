88dca2abac1d694b0595d355e585c791dc6a9e2f
DEV-2 <dev2@gmail.com>
2025-03-20 01:42:09 +0500

    just checking

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports.protect = (...roles) => trycatch(async (req, res, next) => {
     const { id, role } = await jwt.verify(jwtoken, process.env.JWT_SECRET);
     if (roles.includes(role)) {
         const user = await User.findOne({ _id: id, role });
-        if (!user || !user.isActive) return res.status(401).json({ authorized: false });
+        if (!user ||  !user.isActive) return res.status(401).json({ authorized: false });
         req.user = user;
         next();
 
